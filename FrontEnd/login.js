d3b9c1c8a1076de593eff9bf7f43d11fe9c9a3ca
Sylvain JEANPIERRE <sylvain.jeanpierre@gmail.com>
2023-12-05 17:19:20 +0100

    test

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,18 +1,21 @@
-
 const loginForm = document.querySelector("#login-form");
 const emailInput = document.querySelector("#email-input");
 const passwordInput = document.querySelector("#password-input");
-const loginFailed = document.createElement("p")
+const loginFailed = document.createElement("p");
+
+
+function loginSuccess() {
+    window.location.href = "/FrontEnd/index.html";
+}
 
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
     if (emailInput.value === "test" && passwordInput.value === "test") {
-        console.log("test ok")
-        window.location.href = "/FrontEnd/index.html";
+        
     }
     else {
-        if (loginFailed != "Echec de la connexion"){
-        loginFailed.innerText = "Echec de la connexion";
+        if (loginFailed != "Erreur dans l'identifiant ou le mot de passe"){
+        loginFailed.innerText = "Erreur dans l'identifiant ou le mot de passe";
         loginForm.appendChild(loginFailed)
         }
     }
