bbc80502fd1dd6ab91e0045d72e928e46f3d54c9
Sylvain JEANPIERRE <sylvain.jeanpierre@gmail.com>
2023-12-06 14:41:48 +0100

    login and logout features
    - login with fetch api
    - token recovery

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,19 +1,36 @@
-
 const loginForm = document.querySelector("#login-form");
 const emailInput = document.querySelector("#email-input");
 const passwordInput = document.querySelector("#password-input");
-const loginFailed = document.createElement("p")
+const loginFailed = document.createElement("p");
+
 
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    if (emailInput.value === "test" && passwordInput.value === "test") {
-        console.log("test ok")
-        window.location.href = "/FrontEnd/index.html";
+    const infosLog = {
+        "email": emailInput.value,
+        "password": passwordInput.value
     }
-    else {
-        if (loginFailed != "Echec de la connexion"){
-        loginFailed.innerText = "Echec de la connexion";
-        loginForm.appendChild(loginFailed)
-        }
-    }
+    const infosLogJSON = JSON.stringify(infosLog);
+    const reponse = fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: infosLogJSON
+    });
+    
+    console.log(reponse.json())
+
+
 })
+
+
+
+
+// if (loginFailed != "Erreur dans l'identifiant ou le mot de passe") {
+//     loginFailed.innerText = "Erreur dans l'identifiant ou le mot de passe";
+//     loginForm.appendChild(loginFailed)
+// }
+
+
+
+// sophie.bluel@test.tld
+// S0phie
